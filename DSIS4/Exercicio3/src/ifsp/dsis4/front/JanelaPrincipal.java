4b7972ee8af3cb624ceec54a6412d46ed9ce3153
CaioCresencio <caiocresencio98@gmail.com>
2018-08-04 15:35:39 -0300

    Inicio do exercicio 3: conexao com o bd e janela principal

---
+++ DSIS4/Exercicio3/src/ifsp/dsis4/front/JanelaPrincipal.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifsp.dsis4.front;
+
+import java.awt.event.ActionEvent;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ *
+ * @author caio
+ */
+public class JanelaPrincipal extends JFrame {
+    private JDesktopPane desktop;
+    private JMenu menu;
+    private JMenuItem consultar;
+    private JMenuBar barra;
+    
+    public JanelaPrincipal(){
+        super("Exercicio 3");
+        gerar();
+        configurar();
+    }   
+    private void gerar(){
+        this.desktop = new JDesktopPane();
+        this.menu = new JMenu("Arquivo");
+        this.barra = new JMenuBar();
+        this.consultar = new JMenuItem("Consultar");
+        consultar.addActionListener(this::abrirJanela);
+        
+        
+        menu.add(consultar);
+        barra.add(menu);
+        setJMenuBar(barra);
+        add(desktop);
+    }
+    private void configurar(){
+        setVisible(true);
+        setSize(1200,800);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    private void abrirJanela(ActionEvent e){
+        
+    }
+}
---
+++ DSIS4/Exercicio3/src/ifsp/dsis4/main/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifsp.dsis4.main;
+
+import ifsp.dsis4.banco.ConexaoBD;
+import ifsp.dsis4.front.JanelaPrincipal;
+
+/**
+ *
+ * @author caio
+ */
+public class Main {
+    public static void main(String[] args) {
+        ConexaoBD conexao = ConexaoBD.getInstance();
+        try{
+            conexao.getConnection();
+            System.out.println("Conexão estabelecida!"); 
+        }catch(Exception e){
+            System.out.println("Algo deu errado: "+e.getMessage());
+        }
+        JanelaPrincipal jp = new JanelaPrincipal();
+    }
+}
